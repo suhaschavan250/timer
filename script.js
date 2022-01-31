4941fd0b4ebf19e4a71627abe2e47fbb86cf2370
suhaschavan250 <suhaschavan250@gmail.com>
2022-01-31 18:48:47 +0530

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+let count=0;
+let counter=document.getElementById("counter");
+function minus(){
+    if(count==0){
+        count=0;
+    }else{
+        count=count-1;
+    }
+    counter.innerHTML=count;
+}
+function plus(){
+    count=count+1;
+    
+    counter.innerHTML=count;
+}
